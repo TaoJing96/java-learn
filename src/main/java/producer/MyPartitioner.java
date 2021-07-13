3c5e9fe2dd5dae54faf1986f418dc3b092c1ee7f
taojing <taojing.biu@bytedance.com>
2021-07-13 16:09:11 +0800

    Initial commit

---
+++ src/main/java/producer/MyPartitioner.java
@@ -0,0 +1,26 @@
+package producer;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+public class MyPartitioner implements Partitioner {
+    @Override
+    public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+        if ("topic1".equals(s)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
