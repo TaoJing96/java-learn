c6ea66ecf381a706c9253594cc546b8eeef5d6cb
taojing <taojing.biu@bytedance.com>
2021-07-19 21:41:20 +0800

    ADD: 待完善nio demo

---
+++ src/main/java/nio/demo/client.java
@@ -0,0 +1,17 @@
+package nio.demo;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 8088);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String next = scanner.next();
+            System.out.println("send:" + next);
+            socket.getOutputStream().write(next.getBytes());
+        }
+    }
+}
