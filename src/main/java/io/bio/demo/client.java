78a8ffbda4c0c2f47cb221202fbee9218efbb130
taojing <taojing.biu@bytedance.com>
2021-07-20 11:15:36 +0800

    MOD: structure

--- src/main/java/io/bio/demo/client.java
+++ src/main/java/io/bio/demo/client.java
@@ -1,4 +1,4 @@
-package bio.demo;
+package io.bio.demo;
 
 import java.io.IOException;
 import java.net.Socket;
--- src/main/java/io/nio/demo/server.java
+++ src/main/java/io/nio/demo/server.java
@@ -1,4 +1,4 @@
-package nio.demo;
+package io.nio.demo;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
