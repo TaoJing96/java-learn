d5bb232d1ec49b206aec164f3c543e3d6b8a052e
taojing <taojing.biu@bytedance.com>
2021-07-20 15:45:45 +0800

    ADD: nio client server demo

---
+++ src/main/java/io/nio/simple_demo/server.java
@@ -0,0 +1,38 @@
+package io.nio.simple_demo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+import java.util.List;
+
+public class server {
+    private static List<SocketChannel> socketChannelList = new LinkedList<>();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress("127.0.0.1", 8088));
+        serverSocketChannel.configureBlocking(false);//非阻塞
+        while (true) {
+            SocketChannel socketChannel = serverSocketChannel.accept();//非阻塞情况下会立即返回
+            if (socketChannel != null) {
+                System.out.println("get conn:" + socketChannel.getLocalAddress().toString());
+                socketChannelList.add(socketChannel);
+            } else {
+                System.out.println("no conn");
+            }
+            for (SocketChannel channel : socketChannelList) {
+                int read = channel.read(byteBuffer);
+                byteBuffer.flip();//写转读
+                if (read <= 0) {
+                    System.out.println("no data");
+                } else {
+                    System.out.println("get data from:" + channel.getLocalAddress().toString() + " data:" + new String(byteBuffer.array()));
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+}
