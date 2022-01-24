7f3c1adb60aeb9594444ace834b319fba2470dd8
taojing <taojing.biu@bytedance.com>
2022-01-24 20:41:21 +0800

    ADD: test

--- src/main/java/interview/线程交替输出abc_condition.java
+++ src/main/java/interview/线程交替输出abc_condition.java
@@ -1,5 +1,7 @@
 package interview;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -65,5 +67,59 @@ public class 线程交替输出abc_condition {
                 lock.unlock();
             }
         }, "t1").start();
+    }
+}
+
+//利用阻塞队列
+class solution2 {
+
+    private static final BlockingQueue<Integer> queue1 = new ArrayBlockingQueue<>(1);
+
+    private static final BlockingQueue<Integer> queue2 = new ArrayBlockingQueue<>(1);
+
+    private static final BlockingQueue<Integer> queue3 = new ArrayBlockingQueue<>(1);
+
+    public static void main(String[] args) {
+        queue1.offer(1);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    queue1.take();
+                    System.out.println(1 + i * 10);
+                    Thread.sleep(100);
+                    queue2.offer(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    queue2.take();
+                    System.out.println(2+ i * 10);
+                    Thread.sleep(100);
+                    queue3.offer(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    queue3.take();
+                    System.out.println(3+ i * 10);
+                    System.out.println("-------------");
+                    Thread.sleep(100);
+                    queue1.offer(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 class TreeNode {
     int val;
@@ -46,13 +47,28 @@ class ListNode {
 
 class test {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode();
-        TreeNode node2 = new TreeNode();
-        TreeNode node3 = new TreeNode();
-        TreeNode node4 = new TreeNode();
-        node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
 }
 
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int ans = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                ans = Math.max(ans, height[l] * (r - l));
+                l++;
+            } else {
+                ans = Math.max(ans, height[r] * (r - l));
+                r--;
+            }
+        }
+        return ans;
+    }
+}
