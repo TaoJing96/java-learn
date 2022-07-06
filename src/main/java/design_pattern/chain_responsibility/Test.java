6ed6ed3d84731e7de94434cb0cee5bff0f968a75
jingtao <2205674160@qq.com>
2022-07-06 20:20:21 +0800

    add: 责任链

---
+++ src/main/java/design_pattern/chain_responsibility/Test.java
@@ -0,0 +1,13 @@
+package design_pattern.chain_responsibility;
+
+/**
+ * @author jingtao
+ * @date 2022/7/6 8:19 PM
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //servlet chain
+        //quotes chain ra rg sra od每个都是一条链，结合模板方法模式
+    }
+}
